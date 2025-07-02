37ecb8d595b0dea97bb678f5aa8ff49b4f039458
JohnFrancisQuinzon <quinzonjohnfrancis01@gmail.com>
2025-07-02 18:16:31 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,14 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1>HELLO WORLD V16</h1>
-      <h1>HELLO WORLD V16</h1>
-      <h1>HELLO WORLD V16</h1>
-      <h1>HELLO WORLD V16</h1>
-      <h1>HELLO WORLD V16</h1>
-      <h1>HELLO WORLD V16</h1>
-      <h1>HELLO WORLD V16</h1>
-      <h1>HELLO WORLD V16</h1>
-      <h1>HELLO WORLD V16</h1>
-      <h1>HELLO WORLD V16</h1>
+      <h1>HELLO WORLD V17</h1>
+      <h1>HELLO WORLD V17</h1>
+      <h1>HELLO WORLD V17</h1>
+      <h1>HELLO WORLD V17</h1>
+      <h1>HELLO WORLD V17</h1>
+      <h1>HELLO WORLD V17</h1>
+      <h1>HELLO WORLD V17</h1>
+      <h1>HELLO WORLD V17</h1>
     </div>
   )
 }
